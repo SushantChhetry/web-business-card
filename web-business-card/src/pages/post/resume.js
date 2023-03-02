915b29d88fc0a57af4c16ae086a0690022de2429
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-03-01 21:53:03 -0600

    Basic pages set up

---
+++ web-business-card/src/pages/post/resume.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+const resume = () => {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+};
+
+export default resume;
